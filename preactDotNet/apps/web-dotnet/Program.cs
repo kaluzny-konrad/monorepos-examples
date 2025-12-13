3c4a1a2673bcbb904d59e1b880a2b4921296ce28
Konrad Zielinski <konrad.kaluzny@hotmail.com>
2025-12-13 15:55:22 +0100

    init app

--- preactDotNet/apps/web-dotnet/Program.cs
+++ preactDotNet/apps/web-dotnet/Program.cs
@@ -7,11 +7,11 @@ app.MapGet("/", () =>
 {
     var html = RenderPreact("Button", new { text = "Hello from Preact" });
 
-    return Results.Text($"""
+    return Results.Text($$"""
         <html>
           <body>
-            <div data-preact data-component="Button" data-props='{{"text": "Hello from Preact"}}'>
-              {html}
+            <div data-preact data-component="Button" data-props='{"text": "Hello from Preact"}'>
+              {{html}}
             </div>
 
             <script type="module" src="/preact/client.js"></script>
@@ -24,15 +24,20 @@ app.Run();
 
 string RenderPreact(string component, object props)
 {
+    var jsonProps = System.Text.Json.JsonSerializer.Serialize(props);
     var psi = new ProcessStartInfo
     {
         FileName = "node",
-        Arguments = $"ssr.js {component} '{System.Text.Json.JsonSerializer.Serialize(props)}'",
-        RedirectStandardOutput = true
+        Arguments = $"ssr.js {component}",
+        RedirectStandardOutput = true,
+        RedirectStandardInput = true,
+        UseShellExecute = false
     };
 
     psi.WorkingDirectory = Directory.GetCurrentDirectory();
 
     using var process = Process.Start(psi)!;
+    process.StandardInput.WriteLine(jsonProps);
+    process.StandardInput.Close();
     return process.StandardOutput.ReadToEnd();
 }
--- preactDotNet/apps/web-dotnet/ssr.js
+++ preactDotNet/apps/web-dotnet/ssr.js
@@ -1,6 +1,8 @@
 import { render } from "../../packages/ui-preact/dist/server.cjs";
+import { readFileSync } from "fs";
 
 const component = process.argv[2];
-const props = JSON.parse(process.argv[3]);
+const jsonProps = readFileSync(0, "utf-8").trim();
+const props = JSON.parse(jsonProps);
 
 process.stdout.write(render(component, props));
