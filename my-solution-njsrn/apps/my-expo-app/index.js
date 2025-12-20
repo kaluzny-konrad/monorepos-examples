d33db5e9b86935a81d06d07a6f9589c555dabc81
Konrad Zielinski <konrad.kaluzny@hotmail.com>
2025-12-20 18:40:56 +0100

    clean other solution

--- my-solution-njsrn/apps/my-expo-app/index.js
+++
@@ -1,7 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-console.log('Index.js loaded, registering App component...');
-registerRootComponent(App);
-console.log('App component registered');
-
